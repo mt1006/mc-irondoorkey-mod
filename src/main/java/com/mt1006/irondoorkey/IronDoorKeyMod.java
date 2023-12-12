c797dd8483c04c3573f266e111df16bb327af68e
mt1006 <damianm916@gmail.com>
2023-12-13 00:22:36 +0100

    Version for 1.20.4

--- src/main/java/com/mt1006/irondoorkey/IronDoorKeyMod.java
+++ src/main/java/com/mt1006/irondoorkey/IronDoorKeyMod.java
@@ -14,7 +14,7 @@ public class IronDoorKeyMod implements ModInitializer
 {
 	public static final String MOD_ID = "irondoorkey";
 	public static final String VERSION = "1.0";
-	public static final String FOR_VERSION = "1.20.2";
+	public static final String FOR_VERSION = "1.20.4";
 	public static final String FOR_LOADER = "Fabric";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
