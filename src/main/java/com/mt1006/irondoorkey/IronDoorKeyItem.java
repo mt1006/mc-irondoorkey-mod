e30319947aa58af6c47c114ab3ccbb1ed2e78a04
mt1006 <damianm916@gmail.com>
2024-05-25 21:09:14 +0200

    Version 1.1

--- src/main/java/com/mt1006/irondoorkey/IronDoorKeyItem.java
+++ src/main/java/com/mt1006/irondoorkey/IronDoorKeyItem.java
@@ -1,6 +1,7 @@
 package com.mt1006.irondoorkey;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
@@ -8,8 +9,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.FenceGateBlock;
 import net.minecraft.world.level.block.TrapDoorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -29,33 +30,31 @@ public class IronDoorKeyItem extends Item
 		Level level = ctx.getLevel();
 		BlockPos blockPos = ctx.getClickedPos();
 		BlockState blockState = level.getBlockState(blockPos);
+
+		if (!blockState.is(IronDoorKeyMod.OPENABLE)) { return InteractionResult.PASS; }
 		Block blockType = blockState.getBlock();
 
 		if (blockType instanceof DoorBlock)
 		{
-			if (blockType == Blocks.IRON_DOOR)
-			{
-				openDoor(ctx.getPlayer(), level, blockPos, blockState);
-				return InteractionResult.sidedSuccess(level.isClientSide);
-			}
+			DoorBlock doorBlock = (DoorBlock)blockType;
+			doorBlock.setOpen(ctx.getPlayer(), level, blockState, blockPos, !doorBlock.isOpen(blockState));
+			return InteractionResult.sidedSuccess(level.isClientSide);
 		}
 		else if (blockType instanceof TrapDoorBlock)
 		{
-			if (blockType == Blocks.IRON_TRAPDOOR)
-			{
-				openTrapDoor(ctx.getPlayer(), level, blockPos, blockState);
-				return InteractionResult.sidedSuccess(level.isClientSide);
-			}
+			openTrapDoor(ctx.getPlayer(), level, blockPos, blockState);
+			return InteractionResult.sidedSuccess(level.isClientSide);
 		}
-		return InteractionResult.PASS;
-	}
+		else if (blockType instanceof FenceGateBlock)
+		{
+			// redundant in vanilla, added for better mod support, e.g. with SecurityCraft
+			openFenceGate(ctx.getPlayer(), level, blockPos, blockState);
+			return InteractionResult.sidedSuccess(level.isClientSide);
+		}
 
-	private static void openDoor(@Nullable Player player, Level level, BlockPos blockPos, BlockState blockState)
-	{
-		BlockState newBlockState = blockState.cycle(DoorBlock.OPEN);
-		level.setBlock(blockPos, newBlockState, 2);
-		level.levelEvent(player, newBlockState.getValue(DoorBlock.OPEN) ? 1005 : 1011, blockPos, 0);
-		level.gameEvent(player, newBlockState.getValue(DoorBlock.OPEN) ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, blockPos);
+		IronDoorKeyMod.LOGGER.warn("Failed to open the block - " +
+				"it has \"openable\" tag, but isn't instance of DoorBlock or TrapDoorBlock");
+		return InteractionResult.PASS;
 	}
 
 	private static void openTrapDoor(@Nullable Player player, Level level, BlockPos blockPos, BlockState blockState)
@@ -70,6 +69,26 @@ public class IronDoorKeyItem extends Item
 
 		boolean isOpen = newBlockState.getValue(TrapDoorBlock.OPEN);
 		level.levelEvent(player, isOpen ? 1037 : 1036, blockPos, 0);
+		level.gameEvent(player, isOpen ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, blockPos);
+	}
+
+	private static void openFenceGate(@Nullable Player player, Level level, BlockPos blockPos, BlockState blockState)
+	{
+		boolean wasOpen = blockState.getValue(FenceGateBlock.OPEN);
+		if (wasOpen)
+		{
+			BlockState newBlockState = blockState.setValue(FenceGateBlock.OPEN, false);
+			level.setBlock(blockPos, newBlockState, 10);
+		}
+		else
+		{
+			Direction direction = player != null ? player.getDirection() : blockState.getValue(FenceGateBlock.FACING);
+			BlockState newBlockState = blockState.setValue(FenceGateBlock.OPEN, true).setValue(FenceGateBlock.FACING, direction);
+			level.setBlock(blockPos, newBlockState, 10);
+		}
+
+		boolean isOpen = !wasOpen;
+		level.levelEvent(player, isOpen ? 1008 : 1014, blockPos, 0);
 		level.gameEvent(player, isOpen ? GameEvent.BLOCK_OPEN : GameEvent.BLOCK_CLOSE, blockPos);
 	}
 }
