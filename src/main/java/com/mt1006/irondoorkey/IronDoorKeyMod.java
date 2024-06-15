c34e2e30a5b1f5d1146987827e2a127376582bdb
mt1006 <damianm916@gmail.com>
2024-06-15 22:16:21 +0200

    Version 1.21

--- src/main/java/com/mt1006/irondoorkey/IronDoorKeyMod.java
+++ src/main/java/com/mt1006/irondoorkey/IronDoorKeyMod.java
@@ -19,13 +19,13 @@ public class IronDoorKeyMod
 {
 	public static final String MOD_ID = "irondoorkey";
 	public static final String VERSION = "1.1";
-	public static final String FOR_VERSION = "1.20.6";
+	public static final String FOR_VERSION = "1.21";
 	public static final String FOR_LOADER = "Forge";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, IronDoorKeyMod.MOD_ID);
 	public static final RegistryObject<Item> ITEM_IRON_DOOR_KEY = ITEMS.register("iron_door_key", IronDoorKeyItem::new);
-	public static final TagKey<Block> OPENABLE = TagKey.create(Registries.BLOCK, new ResourceLocation(MOD_ID, "openable"));
+	public static final TagKey<Block> OPENABLE = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(MOD_ID, "openable"));
 
 	public IronDoorKeyMod()
 	{
