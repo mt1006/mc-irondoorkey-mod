41ebaa84732f7c6ea9d03ce12db127e3883ab636
mt1006 <damianm916@gmail.com>
2024-09-11 23:36:14 +0200

    Version 1.1.1

--- src/main/java/com/mt1006/irondoorkey/IronDoorKeyMod.java
+++ src/main/java/com/mt1006/irondoorkey/IronDoorKeyMod.java
@@ -5,10 +5,12 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
@@ -17,23 +19,23 @@ import org.slf4j.Logger;
 public class IronDoorKeyMod
 {
 	public static final String MOD_ID = "irondoorkey";
-	public static final String VERSION = "1.1";
-	public static final String FOR_VERSION = "1.21";
-	public static final String FOR_LOADER = "NeoForge";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, MOD_ID);
-	public static final DeferredHolder<Item, IronDoorKeyItem> ITEM_IRON_DOOR_KEY = ITEMS.register("iron_door_key", IronDoorKeyItem::new);
+	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, MOD_ID);
+	private static final DeferredHolder<Item, IronDoorKeyItem> ITEM_IRON_DOOR_KEY = ITEMS.register("iron_door_key", IronDoorKeyItem::new);
 	public static final TagKey<Block> OPENABLE = TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(MOD_ID, "openable"));
 
 	public IronDoorKeyMod(IEventBus eventBus)
 	{
-		LOGGER.info("{} - Author: mt1006", getFullName());
 		ITEMS.register(eventBus);
+		eventBus.addListener(this::creativeModeTabSetup);
 	}
 
-	public static String getFullName()
+	private void creativeModeTabSetup(BuildCreativeModeTabContentsEvent event)
 	{
-		return "IronDoorKey v" + VERSION + " for Minecraft " + FOR_VERSION + " [" + FOR_LOADER + "]";
+		if (event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES))
+		{
+			event.accept(IronDoorKeyMod.ITEM_IRON_DOOR_KEY.get());
+		}
 	}
 }
