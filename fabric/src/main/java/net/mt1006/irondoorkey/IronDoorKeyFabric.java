515c588d4a58724aa8b29209422ba42ddf051c60
mt1006 <mt1006@proton.me>
2026-03-25 00:35:21 +0100

    Port to 26.1

--- fabric/src/main/java/net/mt1006/irondoorkey/IronDoorKeyFabric.java
+++ fabric/src/main/java/net/mt1006/irondoorkey/IronDoorKeyFabric.java
@@ -1,7 +1,7 @@
 package net.mt1006.irondoorkey;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -14,6 +14,6 @@ public class IronDoorKeyFabric implements ModInitializer
 	@Override public void onInitialize()
 	{
 		Registry.register(BuiltInRegistries.ITEM, IronDoorKeyItem.ID, ITEM_IRON_DOOR_KEY);
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register((content) -> content.accept(ITEM_IRON_DOOR_KEY));
+		CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register((content) -> content.accept(ITEM_IRON_DOOR_KEY));
 	}
 }
